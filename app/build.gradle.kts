a43dca9c9c64f7bd1982a45588dc738cc43c8900
Akexorcist <akexorcist@gmail.com>
2023-11-17 02:18:08 +0700

    Use Play Services Instance Apps v18.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation(project(path = ":screenChecker"))
-    implementation("com.google.android.gms:play-services-instantapps:17.0.0")
+    implementation("com.google.android.gms:play-services-instantapps:18.0.1")
 }
