0e35cab5b6a7c620654c9d99352754a739189f1f
Akexorcist <akexorcist@gmail.com>
2023-11-18 03:15:29 +0700

    Update to version 2.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "app.akexorcist.checkscreen"
         minSdk = 21
         targetSdk = 34
-        versionCode = 213
-        versionName = "2.3.0"
+        versionCode = 214
+        versionName = "2.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
