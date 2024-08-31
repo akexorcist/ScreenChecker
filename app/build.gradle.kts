2553ad0b46bbe77caf58bbb12af4cf5f315fad28
Akexorcist <akexorcist@gmail.com>
2024-08-31 21:47:53 +0700

    Bump version to 2.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "app.akexorcist.checkscreen"
         minSdk = 21
         targetSdk = 35
-        versionCode = 215
-        versionName = "2.3.2"
+        versionCode = 216
+        versionName = "2.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
